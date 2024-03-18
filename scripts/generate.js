34e72b69f1beee3bf9f8b76663582056579b3c72
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2024-03-18 23:54:26 +0100

    🧪

--- scripts/generate.js
+++ scripts/generate.js
@@ -5,6 +5,14 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ defaultViewport: null });
     const page = await browser.newPage();
 
+    /*
+    await page.setViewport({
+        width: 1425,
+        height: 703,
+        deviceScaleFactor: 1,
+    });
+    */
+
     // Go to your HTML page
     await page.goto('http://192.168.0.120:8080/', {
         waitUntil: 'networkidle0' // Wait for all network connections to finish
@@ -15,8 +23,8 @@ const puppeteer = require('puppeteer');
     await page.pdf({
         path: 'mediakit.pdf', // Output file path
         printBackground: true, // Print background graphics,
-        width: '1123px',
-        height: '794px',
+        width: '1425px',
+        height: '703px',
     });
 
     await browser.close();
